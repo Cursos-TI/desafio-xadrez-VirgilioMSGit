24ddb884ebf352a3c2dc03a980a07ea0c1294a44
VirgilioMSGit <vmsvrb@gmail.com>
2025-07-31 22:58:35 +0000

    NIVEL AVENTUREIRO

--- xadrez.c
+++ xadrez.c
@@ -35,5 +35,29 @@ int main() {
         contadorRainha++;
     } while (contadorRainha < casasRainha);
 
+    // Simulação do Cavalo
+    // O cavalo se move 2 casas para baixo e 1 para a esquerda (em "L")
+    printf("\nMovimento do Cavalo:\n");
+
+    // Definindo o número de movimentos que o cavalo fará
+    int movimentosCavalo = 3; // Número total de movimentos em "L"
+    int casasParaBaixo = 2;
+    int casasParaEsquerda = 1;
+
+    // Loop externo for: repete o movimento completo em "L"
+    for (int i = 0; i < movimentosCavalo; i++) {
+        // Loop interno for: move duas casas para baixo
+        for (int j = 0; j < casasParaBaixo; j++) {
+            printf("Baixo\n");
+        }
+
+        // Loop interno while: move uma casa para a esquerda
+        int esquerda = 0;
+        while (esquerda < casasParaEsquerda) {
+            printf("Esquerda\n");
+            esquerda++;
+        }
+    }
+
     return 0;
 }
